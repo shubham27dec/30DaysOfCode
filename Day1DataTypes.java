e0996e4fa1ee45bc922d5667c2c8e4a5565bae64
Ashish <42469420+ashishgarg880@users.noreply.github.com>
2022-10-15 01:46:08 +0530

    Update Day1DataTypes.java
    Its use full for future time.

--- Day1DataTypes.java
+++ Day1DataTypes.java
@@ -7,6 +7,7 @@ public class Day1DataTypes {
         int i = 4;
         double d = 4.0d;
         String s = "HackerRank ";
+        String s1 = "45";
         Scanner sc = new Scanner(System.in);
         int i1 = sc.nextInt();
         double d1 = sc.nextDouble();
@@ -15,6 +16,8 @@ public class Day1DataTypes {
         System.out.println(i + i1);
         System.out.println(d + d1);
         System.out.println(s + s1);
+        System.out.print(s + i); // HackerRank 4
+        System.out.print(s1 - i); // Its not concat the code subtract the code 45 - 4 = 41;
 
     }
 }
